f016e8d5e872d520555d31678ba5a1fbf61a8244
Farvez777 <jmfarvez14@gmail.com>
2026-04-06 21:55:19 +0530

    updated code

--- testcode.py
+++ testcode.py
@@ -1,4 +1,3 @@
-name = input("what is your name:\n")
-result = print(f"My name is {name}")
-age=int(input("what is your age?\n"))
-ar=print(f"my age is {age}\n")
+
+print("YEAHHHH I HAVE UPDATED MY CODE")
+print("SEE YEA!!!!")
